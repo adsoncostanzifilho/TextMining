c6751f99a4582fdc27280e85344ed409561aab8b
Alysson A. Costa <alysson.avila.costa@gmail.com>
2020-10-29 14:12:45 +0000

    Add Docker deployment setup. Enhance deployment instructions.

---
+++ app/entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [[ -z "${API_KEY}" ]] || [[ -z "${API_SECRET}" ]] || [[ -z "${ACCESS_TOKEN}" ]] || [[ -z "${ACCESS_SECRET}" ]] || [[ -z "${APP_NAME}" ]]; then
+  echo "Credential variables not set properly. Exiting."
+  exit 1;
+else
+  echo "Creating twitter_credentials.R file."
+  mv credentials/twitter_credentials_env_template.R credentials/twitter_credentials.R
+
+  echo "Running application."
+  Rscript app.R
+fi
